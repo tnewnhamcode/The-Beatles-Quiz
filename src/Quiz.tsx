0e9c75c94e72a85f5c856af3e3b675cde7ca8fcd
tnewnhamcode <148156667+tnewnhamcode@users.noreply.github.com>
2025-11-23 12:10:59 +0000

    Update Quiz.tsx

--- src/Quiz.tsx
+++ src/Quiz.tsx
@@ -5,7 +5,7 @@ function Quiz(){
    //accessing the name property in the state object and calling it playerName
    //saying or nothing basically la, incase theres nothing in there so it dont crash.. idk
 return(
-<div className="nonhomescreen">
+<div>
   
 <h1>George Harrison played the sitar? </h1>
 <Link to="/correct">Yes</Link>
@@ -17,5 +17,6 @@ return(
 
 
 export default Quiz;
+
 
 
